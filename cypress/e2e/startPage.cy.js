a55e8ffa955a1f5596e710feab19d3134629d14e
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-11-16 13:50:44 -0500

    AT_01.01.04 and AT_01.01.05 button

--- cypress/e2e/startPage.cy.js
+++ cypress/e2e/startPage.cy.js
@@ -27,7 +27,19 @@ describe('Start page with login', () => {
         startPage.getPasswordInputField()
             .should('be.visible')
             .and('have.attr', 'placeholder', startData.passwordInputFieldPlaceholder);
-    })    
+    })   
+    
+    it('AT_01.01.04 | Verify that login button is visible and has text "Login"', () => {
+        startPage.getLoginBtn()
+        .should('be.visible')
+        .and('have.value', startData.textBtn)
+    })
+
+    it('AT_01.01.05 | Verify that login button color is rgb(19, 35, 34) and background-color is rgb(61, 220, 145)', () => {
+        startPage.getLoginBtn()
+        .should('have.css', 'color', startData.colorBtn)
+        .and('have.css', 'background-color', startData.backgroundColorBtn)
+    })
 
     it('AT_01.01.06 | Verify that usernames header is visible and has text "Accepted usernames are:"', () => {
         startPage.getUsernamesListHeader()
