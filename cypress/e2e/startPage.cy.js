b7c9ec912b35a44794044c23902fcb20e3ca2491
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2023-11-16 13:15:41 -0500

    Login Input fields placeholders (#2)

--- cypress/e2e/startPage.cy.js
+++ cypress/e2e/startPage.cy.js
@@ -1,13 +1,28 @@
 /// <reference types = "Cypress">
 
 import StartPage from "../pageObgect/StartPage";
+import startData from "../fixtures/startData.json";
 
 const startPage = new StartPage();
 
 describe('Start page woth login', () => {
-    it('Verify that start page has header "Swag Labs', () => {
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/');
-        
-        startPage.getMainHeader().should('have.text', "Swag Labs");        
+    })
+
+    it('Verify that start page has header "Swag Labs', () => {        
+
+        startPage.getMainHeader().should('be.visible')
+            .and('have.text', startData.mainHeader);
     });
+
+    it('Verify that user name input field is visible and has placeholder "Username"', () => {
+        startPage.getUsernameInputField().should('be.visible')
+        .and('have.attr', 'placeholder', startData.userNameInputFieldPlaceholder);
+    })
+
+    it('Verify that password input field is visible and has placeholder "Password"', () => {
+        startPage.getPasswordInputField().should('be.visible')
+        .and('have.attr', 'placeholder', startData.passwordInputFieldPlaceholder);
+    })
 });
