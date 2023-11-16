9fa0f25aa647847d257cd81e131971dbffc8126d
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-11-16 13:32:27 -0500

    add AT_01.01.06 and AT_01.01.07

--- cypress/e2e/startPage.cy.js
+++ cypress/e2e/startPage.cy.js
@@ -5,24 +5,40 @@ import startData from "../fixtures/startData.json";
 
 const startPage = new StartPage();
 
-describe('Start page woth login', () => {
+describe('Start page with login', () => {
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com/');
     })
 
-    it('Verify that start page has header "Swag Labs', () => {        
+    it('AT_01.01.01 | Verify that start page has header "Swag Labs', () => {
 
-        startPage.getMainHeader().should('be.visible')
+        startPage.getMainHeader()
+            .should('be.visible')
             .and('have.text', startData.mainHeader);
     });
 
-    it('Verify that user name input field is visible and has placeholder "Username"', () => {
-        startPage.getUsernameInputField().should('be.visible')
-        .and('have.attr', 'placeholder', startData.userNameInputFieldPlaceholder);
+    it('AT_01.01.02 | Verify that user name input field is visible and has placeholder "Username"', () => {
+        startPage.getUsernameInputField()
+            .should('be.visible')
+            .and('have.attr', 'placeholder', startData.userNameInputFieldPlaceholder);
     })
 
-    it('Verify that password input field is visible and has placeholder "Password"', () => {
-        startPage.getPasswordInputField().should('be.visible')
-        .and('have.attr', 'placeholder', startData.passwordInputFieldPlaceholder);
+    it('AT_01.01.03 | Verify that password input field is visible and has placeholder "Password"', () => {
+        startPage.getPasswordInputField()
+            .should('be.visible')
+            .and('have.attr', 'placeholder', startData.passwordInputFieldPlaceholder);
+    })    
+
+    it('AT_01.01.06 | Verify that usernames header is visible and has text "Accepted usernames are:"', () => {
+        startPage.getUsernamesListHeader()
+            .should('be.visible')
+            .and('have.text', startData.namesHeader)
     })
+
+    it('AT_01.01.07 | Verify that password header is visible and has text "Password for all users:"', () => {
+        startPage.getPasswordHeader()
+            .should('be.visible')
+            .and('have.text', startData.passwordHeader)
+    })
+
 });
