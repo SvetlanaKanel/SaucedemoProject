e1393d939c6a6ba495d82fef2a2f1ddb50a10791
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2023-11-18 20:59:22 -0500

    AT_01.01.08 - list of usernames (#10)

--- cypress/pageObgect/StartPage.js
+++ cypress/pageObgect/StartPage.js
@@ -6,8 +6,19 @@ class StartPage {
     getLoginBtn = () => cy.get('#login-button');
     getUsernamesListHeader = () => cy.get('#login_credentials h4');
     getPasswordHeader = () => cy.get('.login_password h4');
+    getUsernamesListString = () => cy.get('#login_credentials')
 
 
     //methods
+    //this method froms Array of usernames from usernames string and returns the name by the index
+    getUsernamesArray(idx) {
+        return this.getUsernamesListString().then($str => {
+            let usernamesArray = $str.text().split(':');            
+            let newUsernamesArray = usernamesArray[1].split('user').map(el => el + "user");
+            newUsernamesArray.pop();
+
+            return newUsernamesArray[idx];
+        })
+    }
 }
 export default StartPage
