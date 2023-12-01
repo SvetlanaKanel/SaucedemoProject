5c055dde09748a37b1cb3b879f3dce517a3ef056
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-12-01 12:50:08 -0500

    AT_02.01.06

--- cypress/pageObgect/ProductPage.js
+++ cypress/pageObgect/ProductPage.js
@@ -6,15 +6,18 @@ class ProductPage {
     getMainHeader = () => cy.get('.title');
     getAllItemsPictures = () => cy.get('.inventory_item img');
     getLogo = () => cy.get('.app_logo');
-    getBurgerMenu = () => cy.get('.bm-burger-button img');
+    getBurgerMenu = () => cy.get('.bm-burger-button');
+    getBurgerMenuLogo = () => cy.get('.bm-burger-button img');
+    getSidebarMenu = () => cy.get('.bm-menu-wrap');
     getSidebarList = () => cy.get('.bm-item-list a');
     getAboutSidebarLink = () => cy.get('#about_sidebar_link');
     getLogoutSidebarLink = () => cy.get('#logout_sidebar_link');
+    getSidebarCrossBtn = () => cy.get('#react-burger-cross-btn');
 
 
     //methods
     clickBurgerMenu() {
-        this.getBurgerMenu().click({force : true});
+        this.getBurgerMenu().click()//.trigger('click');
         return this;
     }
 
@@ -26,6 +29,11 @@ class ProductPage {
     clickLogoutSidebarLink() {
         this.getLogoutSidebarLink().click({force : true}); 
         return StartPage;
+    }
+
+    clickSidebarCrossBtn() {
+        this.getSidebarCrossBtn().click({force: true});
+        return this;
     }
 }
 export default ProductPage;
