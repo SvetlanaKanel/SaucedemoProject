127c5f1f2978ea3289d787009afd06f0534edaea
Svetlana Kanel <svetlana.kanel@gmail.com>
2023-12-06 19:00:08 -0500

    AT_02.03.01, AT_02.03.02, AT_02.03.03

--- cypress/pageObgect/CartPage.js
+++ cypress/pageObgect/CartPage.js
@@ -1,6 +1,8 @@
 class CartPage {
     // elements
-    
+    getSecondaryHeader = () => cy.get('.title');
+    getQuantityLabel = () => cy.get('.cart_quantity_label');
+    getDescriptionLabel = () => cy.get('.cart_desc_label');
 
     //methods
 
